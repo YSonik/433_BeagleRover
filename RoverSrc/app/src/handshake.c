65c126bf764952a784d00e2d21f002ced9303f50
Yogesh <yogesh.sonik135@gmail.com>
2024-03-28 21:23:52 -0700

    Implemented sockets for handshake module

--- RoverSrc/app/src/handshake.c
+++ RoverSrc/app/src/handshake.c
@@ -7,127 +7,133 @@
 #include "handshake.h"
 #include "socket.h"
 
-
 #define MESSAGE_LEN 100
+static int server_socket = 0;
+static int client_socket = 0;
+
+static struct sockaddr_in server_address;
+static struct sockaddr_in client_address;
+static struct sockaddr_in broadcast_address;
+
+char receiveBuffer[MESSAGE_LEN];
+char sendBuffer[MESSAGE_LEN];
+
+static void createServerSocket()
+{
+    //Create the server socket.
+    if ((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        printf("Server socket could not be created\n");
+        exit(1);
+    }
+
+    //Initialize the server address struct.
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(atoi(SERVER_PORT));
+
+    //Bind the socket to the server address struct.
+    if (bind(server_socket, (const struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+        printf("Server socket bind failed\n");
+        exit(1);
+    }
+
+    //Enable broadcast.
+    int broadcastEnable = 1;
+    if (setsockopt(server_socket, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable)) == -1) {
+        printf("Server socket options could not be set\n");
+        exit(1);
+    }
+
+    return;
+}
+
+static void serverListen()
+{
+    while(true)
+    {
+        memset(receiveBuffer, '\0', MESSAGE_LEN);  
+        socklen_t client_len = sizeof(client_address);
+
+        //Receive discovery message from client.
+        ssize_t bytesRx = recvfrom(server_socket, (char *)receiveBuffer, MESSAGE_LEN, 0, (struct sockaddr *)&client_address, &client_len);
+        
+        if (bytesRx == -1) {
+            printf("Server could not receive from client\n");
+            exit(1);
+        }
+
+        printf("Server received the following message: %s\n", receiveBuffer);
+    }
+
+    return;
+}
+
+
+static void createClientSocket()
+{
+    //Create the client socket.
+    if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        printf("Client socket could not be created\n");
+        exit(1);
+    }
+
+    //Enable broadcast.
+    int broadcast_enable = 1;
+    if (setsockopt(client_socket, SOL_SOCKET, SO_BROADCAST, &broadcast_enable, sizeof(broadcast_enable)) == -1) {
+        perror("setsockopt (SO_BROADCAST)");
+        exit(1);
+    }
+
+    //Initialize the struct for broadcasting address.
+    memset(&broadcast_address, 0, sizeof(broadcast_address));
+    broadcast_address.sin_family = AF_INET;
+    broadcast_address.sin_port = htons(atoi(SERVER_PORT));
+    broadcast_address.sin_addr.s_addr = inet_addr("192.168.234.255"); // Broadcasting to all devices in the network
+    
+    return;
+}
+
+static void clientSend()
+{
+    //Send the discovery message.
+    memset(sendBuffer, '\0', MESSAGE_LEN);
+    snprintf(sendBuffer, MESSAGE_LEN, "%s", "ip");
+    if (sendto(client_socket, sendBuffer, strlen(sendBuffer), 0, (struct sockaddr*)&broadcast_address, sizeof(broadcast_address)) == -1) {
+        printf("Client could not send to server\n");
+        exit(1);
+    }
+    
+    printf("Client broadcasted the following discovery message: %s\n",sendBuffer);
+
+    return;
+}
+
 
 void Handshake_init(char* ip_buffer, bool isServer)
 {
-    
-    char receiveBuffer[MESSAGE_LEN];
-    char sendBuffer[MESSAGE_LEN];
-    memset(receiveBuffer, '\0', MESSAGE_LEN);
-    memset(sendBuffer, '\0', MESSAGE_LEN);
-
     if(isServer)
     {
-        //Receive discovery broadcast from client.
-        //Socket_init(SERVER_PORT, NULL, CLIENT_PORT, true);
-        //Socket_receive(receiveBuffer);
-        //printf("Server received broadcast: %s\n",receiveBuffer);
+        //Create the server socket.
+        createServerSocket();
 
-        //Respond with server ip.
-        //snprintf(sendBuffer, MESSAGE_LEN, "%s", "192.168.234.12");
-        //Socket_reply_to_last(sendBuffer);
-        int sockfd;
-        struct sockaddr_in servaddr, cliaddr;
-        char buffer[MESSAGE_LEN];
+        //Listen for discovery message. 
+        serverListen();
 
-        // Create UDP socket
-        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-            perror("socket creation failed");
-            exit(EXIT_FAILURE);
-        }
-
-        // Initialize server address structure
-        memset(&servaddr, 0, sizeof(servaddr));
-        servaddr.sin_family = AF_INET;
-        servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // Listen on all network interfaces
-        servaddr.sin_port = htons(atoi(SERVER_PORT));
-
-        // Bind socket
-        if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
-            perror("bind failed");
-            exit(EXIT_FAILURE);
-        }
-
-        // Enable broadcast
-        int broadcastEnable = 1;
-        if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable)) == -1) {
-            perror("setsockopt (SO_BROADCAST)");
-            exit(EXIT_FAILURE);
-        }
-
-        printf("UDP server listening on port %d...\n", atoi(SERVER_PORT));
-
-        while (1) {
-            socklen_t len = sizeof(cliaddr);
-
-            // Receive data from client
-            ssize_t n = recvfrom(sockfd, (char *)buffer, MESSAGE_LEN, 0,
-                                (struct sockaddr *)&cliaddr, &len);
-            if (n == -1) {
-                perror("recvfrom failed");
-                exit(EXIT_FAILURE);
-            }
-
-            // Null terminate received data
-            buffer[n] = '\0';
-
-            // Display received message
-            printf("Received message from %s:%d: %s\n",
-                inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port), buffer);
-        }
-
-        // Close socket
-        close(sockfd);
+        //Close the server socket.
+        close(server_socket);
     }
     else
     {
-        //Send discovery broadcast to server.
-        //Socket_init(CLIENT_PORT, NULL, SERVER_PORT, true);
-        //snprintf(sendBuffer, MESSAGE_LEN, "%s", "ip");
-        //Socket_send(sendBuffer);
+        //Create the server socket.
+        createClientSocket();
 
-        //Listen for server's response.
-        //Socket_receive(receiveBuffer);
-        //printf("Client received response: %s\n",receiveBuffer);
+        //Listen for discovery message. 
+        clientSend();
 
-
-        int sockfd;
-        struct sockaddr_in broadcast_addr;
-        char buffer[MESSAGE_LEN] = "Hello, broadcast world!";
-
-        // Create UDP socket
-        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-            perror("socket");
-            exit(1);
-        }
-
-        // Enable broadcast
-        int broadcast_enable = 1;
-        if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcast_enable, sizeof(broadcast_enable)) == -1) {
-            perror("setsockopt (SO_BROADCAST)");
-            exit(1);
-        }
-
-        // Set up the destination address for broadcasting
-        memset(&broadcast_addr, 0, sizeof(broadcast_addr));
-        broadcast_addr.sin_family = AF_INET;
-        broadcast_addr.sin_port = htons(atoi(SERVER_PORT));
-        broadcast_addr.sin_addr.s_addr = inet_addr("255.255.255.255"); // Broadcasting to all devices in the network
-
-        // Send the data
-        if (sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&broadcast_addr, sizeof(broadcast_addr)) == -1) {
-            perror("sendto");
-            exit(1);
-        }
-
-        printf("Message sent to broadcast address.\n");
-
-        close(sockfd); // Close the socket
-        return;
+        //Close the client socket
+        close(client_socket);
     }
     
-    //Socket_close();
-    //return;
+    return;
 }
