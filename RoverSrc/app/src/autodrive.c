58ca03306c55fdc26f3d73df0f64b6932100104c
Sagardeep Singh Hunjan <Sagardeep.0019@gmail.com>
2024-04-18 21:18:56 -0400

    Add time_utils.h header to autodrive.c

--- RoverSrc/app/src/autodrive.c
+++ RoverSrc/app/src/autodrive.c
@@ -7,6 +7,7 @@
 
 #include "shutdown.h"
 
+#include "utils/time_utils.h"
 #include "hal/distance_sensor.h"
 #include "hal/dfrobot_pirate.h"
 
@@ -25,31 +26,53 @@ static void *AutoDrive_thread()
 {
     while (!Shutdown_isRequested())
     {
+        double distance = DistanceSensor_getDistance();
+
+        if (distance <= 0)
+        {
+            continue;
+        }
+
         if (DistanceSensor_getDistance() < 10)
         {
             speedBeforeStop = DFRobotPirate_getSpeed();
 
-            AutoDrive_stop();
+            DFRobotPirate_stop();
 
             if (!is_running)
             {
                 continue;
             }
 
-            int direction = rand() % 2;
-
-            while (DistanceSensor_getDistance() < 10)
+            while (DistanceSensor_getDistance() < 100)
             {
-                if (direction == 0)
-                {
-                    DFRobotPirate_turnLeft();
-                }else{
-                    DFRobotPirate_turnRight();
-                }
+                DFRobotPirate_moveBackward();
+                DFRobotPirate_setSpeed(MAX_SPEED);
 
-                DFRobotPirate_setSpeed(speedBeforeStop);
+                sleepForMs(100);
             }
 
+            DFRobotPirate_setSpeed(speedBeforeStop);
+
+            int direction = rand() % 2;
+
+            if (direction == 0)
+            {
+                DFRobotPirate_turnLeft();
+            }
+            else
+            {
+                DFRobotPirate_turnRight();
+            }
+
+            sleepForMs(2000);
+
+            DFRobotPirate_setSpeed(speedBeforeStop);
+
+            DFRobotPirate_moveForward();
+        }
+        else if (is_running)
+        {
             DFRobotPirate_moveForward();
         }
     }
